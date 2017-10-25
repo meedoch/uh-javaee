a37de43c121d642310dea29b06ea5135163fae34
aichamhedhbi <aicha.mhedhbi@esprit.tn>
2017-10-25 15:08:13 +0100

    Added ResourcesHistory Entity and Service

--- universalhaven-web/src/main/java/tn/undefined/universalhaven/resources/CallForHelpResource.java
+++ universalhaven-web/src/main/java/tn/undefined/universalhaven/resources/CallForHelpResource.java
@@ -1,5 +1,7 @@
 package tn.undefined.universalhaven.resources;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -18,7 +20,6 @@ import javax.ws.rs.core.Response.Status;
 import tn.undefined.universalhaven.buisness.CallForHelpServiceLocal;
 import tn.undefined.universalhaven.entity.CallForHelp;
 
-
 @Path("callforhelp")
 @RequestScoped
 @ManagedBean
@@ -30,49 +31,64 @@ public class CallForHelpResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response addCallForHelpResource(CallForHelp callForHelp) {
 		if (callForHelpServiceLocal.startCallForHelp(callForHelp))
-			return Response.status(Status.ACCEPTED).build();
-		return Response.status(Status.NOT_ACCEPTABLE).build();
+			return Response.status(Status.ACCEPTED).entity("Call For Help Added").build();
+		return Response.status(Status.NOT_ACCEPTABLE).entity("Call For Help  couldn't be added").build();
 	}
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response listActiveCallForHelpEventsResource() {
-		return Response.ok(callForHelpServiceLocal.listEvents()).build();
+		List<CallForHelp> list = callForHelpServiceLocal.listEvents();
+		if (list.isEmpty() == false)
+			return Response.ok(list).build();
+		else
+			return Response.status(Status.NO_CONTENT).entity("No Calls For Help were found ").build();
+
 	}
 
 	@GET
 	@Path("active")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response listCallForHelpEventsResource() {
-		return Response.ok(callForHelpServiceLocal.listActiveEvents()).build();
+		List<CallForHelp> list = callForHelpServiceLocal.listActiveEvents();
+		if (list.isEmpty())
+			return Response.status(Status.NO_CONTENT).entity("No Calls For Help were found").build();
+
+		return Response.ok(list).build();
 	}
 
 	@GET
 	@Path("{title}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCallForHelpByNameResource(@PathParam("title") String title) {
-		return Response.ok(callForHelpServiceLocal.findCallForHelpByTitle(title)).build();
+		List<CallForHelp> list = callForHelpServiceLocal.findCallForHelpByTitle(title);
+		if (list.isEmpty() == false)
+			return Response.ok(list).build();
+		return Response.status(Status.NO_CONTENT).entity("No Calls For Help were found").build();
 	}
 
 	@GET
 	@Path("description/{str}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getCallForHelpByCriteriaResource(@PathParam("str") String description) {
-		return Response.ok(callForHelpServiceLocal.findCallForHelpByDescription(description)).build();
-		}
+		List<CallForHelp> list = callForHelpServiceLocal.findCallForHelpByDescription(description);
+		if (list.isEmpty() == false)
+			return Response.ok(list).build();
+		return Response.status(Status.NO_CONTENT).entity("No Calls For Help were found").build();
+	}
 
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response modifyCallForHelpResource(CallForHelp callForHelp) {
 		if (callForHelpServiceLocal.modifyCallForHelp(callForHelp))
-			return Response.status(Status.ACCEPTED).build();
-		return Response.status(Status.NOT_ACCEPTABLE).build();
+			return Response.status(Status.ACCEPTED).entity("Call For Help modified successfully").build();
+		return Response.status(Status.NOT_ACCEPTABLE).entity("Call For Help couldn't be modified ").build();
 	}
 
 	@DELETE
 	public Response removeCallForHelpResource(CallForHelp callForHelp) {
 		if (callForHelpServiceLocal.endCallForHelp(callForHelp))
-			return Response.status(Status.ACCEPTED).build();
-		return Response.status(Status.NOT_ACCEPTABLE).build();
+			return Response.status(Status.ACCEPTED).entity("Call For Help removed successfully").build();
+		return Response.status(Status.NOT_ACCEPTABLE).entity("Call For Help couldn't be removed").build();
 	}
 }
